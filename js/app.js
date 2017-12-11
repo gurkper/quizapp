92c9c137b18cf64c0fb4a01f74b0727ef57f845e
Teddy Grönberg <teddy.gronberg@elev.olofstrom.se>
2017-12-11 14:31:03 +0100

    la till frågeobjekt

--- js/app.js
+++ js/app.js
@@ -1,9 +1,75 @@
+var fragor = [
+{
+        fraga:"varför e jag blao",
+        rattsvar: 1,
+        alternativ: ["kanske","föratt","ja","nej"]
+},
+{
+    fraga: "vilken färg hade gustav vasas bil?",
+    rattsvar: 2,
+    alternativ: ["blå","röd","vit","gul"]
+}
+];
+
+console.log(fragor[1].fraga);
+var fragetext = document.getElementById("fraga");
+fragetext.innerhtml = fragor [0].fraga;
+
+var knapp = [0];
+for(t = 0; t < alternativ.length; t++) {
+    
+    var vilken = "alt"+t;
+    console.log(vilken);
+knapp[t] = document. getElementById(vilken);
+knapp[t] = innerhtml
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//gammal kod som jag kan titta på
+/*
 var fraga = document.getElementById("fraga");
+var coolfraga = "hur många lingon fanns det i världen?";
+var alternativ = ["tre", "fyra","sex","tjugo"];
+
+for (i = 0; i < alternativ.length; i++) {
+console.log(alternativ[i]);
+}
+
+
+fraga.innerHTML = coolfraga;
+var knapp = [];
+for(t = 0; t < alternativ.length; t++) {
+    console.log(t);
+    var vilken = "alt"+t;
+    console.log(vilken);
+knapp[t] = document. getElementById(vilken);
+console.log(knapp[t]);
+knapp[t].innerHTML = alternativ[t];
+console.log(alternativ[t]);
+}
+*/
+
+/*
 console.log(fraga.innerHTML);
+
 var knapp = document.getElementById("knapp1");
+
 knapp.addEventListener("click", function() {
 console.log("Ja");
-fraga.innerHTML = "hoppsan hejsan";
-
+fraga.innerHTML = "Hoppsan hejsan";
 });
+*/
+
+
 
